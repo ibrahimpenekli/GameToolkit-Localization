6de45a247d145fa3eed6e9f54e4a1751fe59801b
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-16 02:57:40 +0300

    Localized texture behaviours implemented.

---
+++ Assets/GameToolkit/Localization/Scripts/LocalizedMaterialBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameToolkit.Localization
+{
+    public class LocalizedMaterialBehaviour : LocalizedAssetBehaviour
+    {
+		[SerializeField]
+		private Material m_Material;
+
+		[SerializeField, Tooltip("Texture property name.")]
+		private string m_PropertyName = "_MainTex";
+
+		[SerializeField]
+        private LocalizedTexture m_LocalizedAsset;
+
+		protected override bool IsAssetExist()
+        {
+            return m_LocalizedAsset;
+        }
+
+		protected override void UpdateComponentValue()
+        {
+			if (m_Material)
+			{
+				m_Material.SetTexture(m_PropertyName, m_LocalizedAsset.Value);
+			}
+        }
+    }
+}
---
+++ Assets/GameToolkit/Localization/Scripts/LocalizedRendererBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameToolkit.Localization
+{
+	[RequireComponent(typeof(Renderer))]
+    public class LocalizedRendererBehaviour : LocalizedAssetBehaviour
+    {
+		[SerializeField]
+        private int m_MaterialIndex = 0;
+
+        [SerializeField, Tooltip("Texture property name.")]
+        private string m_PropertyName = "_MainTex";
+
+        [SerializeField]
+        private LocalizedTexture m_LocalizedAsset;
+
+        protected override bool IsAssetExist()
+        {
+            return m_LocalizedAsset;
+        }
+
+        protected override void UpdateComponentValue()
+        {
+			GetComponent<Renderer>().materials[m_MaterialIndex].SetTexture(m_PropertyName, m_LocalizedAsset.Value);
+        }
+
+		private void OnValidate()
+		{
+			m_MaterialIndex = Mathf.Max(0, m_MaterialIndex);
+		}
+    }
+}
