7fe12c72e4450b5666164a2a2b8034fecfa12be9
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-17 21:27:15 +0300

    Minor improvements.

--- Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
+++ Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
@@ -7,7 +7,8 @@ using UnityEditorInternal;
 
 namespace GameToolkit.Localization.Editor
 {
-    public abstract class LocalizedAssetEditor<TAsset> : UnityEditor.Editor where TAsset : class
+    [CustomEditor(typeof(LocalizedAssetBase), editorForChildClasses: true)]
+    public class LocalizedAssetEditor : UnityEditor.Editor
     {
         private const float LanguageFieldWidth = 100;
 
@@ -16,40 +17,50 @@ namespace GameToolkit.Localization.Editor
         private void OnEnable()
         {
             var elements = serializedObject.FindProperty("m_LocaleItems");
-            m_LocaleItemList = new ReorderableList
-            (
-                serializedObject: serializedObject, 
-                elements: elements,
-                draggable: true,
-                displayHeader: true,
-                displayAddButton: true,
-                displayRemoveButton: true
-            );
-            
-            m_LocaleItemList.drawHeaderCallback = (Rect rect) =>
-            {   
-                EditorGUI.LabelField(rect, ObjectNames.NicifyVariableName(target.GetType().Name) + "s");
-            };
-            
-            m_LocaleItemList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => 
+            if (elements != null)
             {
-                var element = m_LocaleItemList.serializedProperty.GetArrayElementAtIndex(index);
-                
-                // Language field.
-                var r1 = new Rect(rect.x, rect.y + 2, LanguageFieldWidth, EditorGUIUtility.singleLineHeight);
-                EditorGUI.PropertyField(r1, element.FindPropertyRelative("m_Language"), GUIContent.none);
+                m_LocaleItemList = new ReorderableList
+                (
+                    serializedObject: serializedObject,
+                    elements: elements,
+                    draggable: true,
+                    displayHeader: true,
+                    displayAddButton: true,
+                    displayRemoveButton: true
+                );
 
-                // Value field.
-                var r2 = new Rect(r1.x + r1.width + 4, r1.y, rect.width - r1.width - 4, r1.height);
-                EditorGUI.PropertyField(r2, element.FindPropertyRelative("m_Value"), GUIContent.none);
-            };
+                m_LocaleItemList.drawHeaderCallback = (Rect rect) =>
+                {
+                    EditorGUI.LabelField(rect, ObjectNames.NicifyVariableName(target.GetType().Name) + "s");
+                };
+
+                m_LocaleItemList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+                {
+                    var element = m_LocaleItemList.serializedProperty.GetArrayElementAtIndex(index);
+
+                    // Language field.
+                    var r1 = new Rect(rect.x, rect.y + 2, LanguageFieldWidth, EditorGUIUtility.singleLineHeight);
+                    EditorGUI.PropertyField(r1, element.FindPropertyRelative("m_Language"), GUIContent.none);
+
+                    // Value field.
+                    var r2 = new Rect(r1.x + r1.width + 4, r1.y, rect.width - r1.width - 4, r1.height);
+                    EditorGUI.PropertyField(r2, element.FindPropertyRelative("m_Value"), GUIContent.none);
+                };
+            }
         }
 
         public override void OnInspectorGUI()
         {
-            serializedObject.Update();
-            m_LocaleItemList.DoLayoutList();
-            serializedObject.ApplyModifiedProperties();
+            if (m_LocaleItemList != null)
+            {
+                serializedObject.Update();
+                m_LocaleItemList.DoLayoutList();
+                serializedObject.ApplyModifiedProperties();
+            }
+            else
+            {
+                base.OnInspectorGUI();
+            }
         }
     }
 }
