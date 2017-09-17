f6848ce1de8cc9c96611c74773dea28d2ea20c47
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-17 12:19:34 +0300

    Minor changes.

--- Assets/GameToolkit/Localization/Scripts/LocalizedMaterialBehaviour.cs
+++ Assets/GameToolkit/Localization/Scripts/LocalizedMaterialBehaviour.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿// Copyright (c) H. Ibrahim Penekli. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
