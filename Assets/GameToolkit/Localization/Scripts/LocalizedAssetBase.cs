330130956c132b6f35e925a2831692eee64460d4
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-20 02:50:20 +0300

    Removed double comment.

--- Assets/GameToolkit/Localization/Scripts/LocalizedAssetBase.cs
+++ Assets/GameToolkit/Localization/Scripts/LocalizedAssetBase.cs
@@ -1,9 +1,6 @@
 // Copyright (c) H. Ibrahim Penekli. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-// Copyright (c) H. Ibrahim Penekli. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
 using System;
 using UnityEngine;
 
