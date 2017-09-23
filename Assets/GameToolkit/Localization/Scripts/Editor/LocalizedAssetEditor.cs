b041dd7a37978b36e37204b5860c62ec587a4d9f
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-23 08:43:38 +0300

    Languages added from settings and SystemLanguage enum implemented. Native language name asset added.

--- Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
+++ Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
@@ -4,6 +4,8 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
+using System.Collections.Generic;
+using System;
 
 namespace GameToolkit.Localization.Editor
 {
@@ -13,16 +15,17 @@ namespace GameToolkit.Localization.Editor
         private const float LanguageFieldWidth = 100;
 
         private ReorderableList m_LocaleItemList;
+        private SerializedProperty m_LocaleItems;
 
         private void OnEnable()
         {
-            var elements = serializedObject.FindProperty(LocalizationEditorHelper.LocalizedElementsSerializedProperty);
-            if (elements != null)
+            m_LocaleItems = serializedObject.FindProperty(LocalizationEditorHelper.LocalizedElementsSerializedProperty);
+            if (m_LocaleItems != null)
             {
                 m_LocaleItemList = new ReorderableList
                 (
                     serializedObject: serializedObject,
-                    elements: elements,
+                    elements: m_LocaleItems,
                     draggable: true,
                     displayHeader: true,
                     displayAddButton: true,
@@ -50,6 +53,26 @@ namespace GameToolkit.Localization.Editor
                 {
                     return list.count > 1;
                 };
+                m_LocaleItemList.onAddDropdownCallback = (Rect buttonRect, ReorderableList list) =>
+                {
+                    var menu = new GenericMenu();
+                    menu.AddItem(new GUIContent("Language", "Adds a language."), false, () =>
+                    {
+                        ReorderableList.defaultBehaviours.DoAddButton(list);
+                        serializedObject.ApplyModifiedProperties();
+                    });
+                    menu.AddItem(new GUIContent("Add languages from settings", "Adds by searching used languages in assets."), false, () =>
+                    {
+                        AddLanguagesFromSettings();
+                        serializedObject.ApplyModifiedProperties();
+                    });
+                    menu.AddItem(new GUIContent("Add all languages", "Adds all languages."), false, () =>
+                    {
+                        AddAllLanguages();
+                        serializedObject.ApplyModifiedProperties();
+                    });
+                    menu.ShowAsContext();
+                };
             }
         }
 
@@ -66,6 +89,57 @@ namespace GameToolkit.Localization.Editor
             else
             {
                 base.OnInspectorGUI();
+            }
+        }
+
+        private void AddAllLanguages()
+        {
+            var enumNames = Enum.GetNames(typeof(SystemLanguage));
+            m_LocaleItems.arraySize = enumNames.Length;
+            for (int i = 0; i < m_LocaleItems.arraySize; i++)
+            {
+                var element = m_LocaleItems.GetArrayElementAtIndex(i);
+                var languageProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleLanguageSerializedProperty);
+                languageProperty.enumValueIndex = i;
+            }
+        }
+
+        private void AddLanguagesFromSettings()
+        {
+            var enumNames = Enum.GetNames(typeof(SystemLanguage));
+            var uniqueLanguages = new HashSet<SystemLanguage>();
+            for (int i = 0; i < m_LocaleItems.arraySize; i++)
+            {
+                var element = m_LocaleItems.GetArrayElementAtIndex(i);
+                var languageProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleLanguageSerializedProperty);
+                var enumName = enumNames[languageProperty.enumValueIndex];
+                uniqueLanguages.Add((SystemLanguage)Enum.Parse(typeof(SystemLanguage), enumName));
+            }
+
+            var availableLanguages = LocalizationSettings.Instance.AvailableLanguages;
+            foreach (var lang in availableLanguages)
+            {
+                uniqueLanguages.Add(lang);
+            }
+
+            var localizedAssets = Localization.Instance.FindAllLocalizedAssets();
+            foreach (var localizedAsset in localizedAssets)
+            {
+                foreach (var locale in localizedAsset.LocaleItems)
+                {
+                    uniqueLanguages.Add(locale.Language);
+                }
+            }
+
+            var languages = new List<SystemLanguage>(uniqueLanguages);
+            m_LocaleItems.arraySize = languages.Count;
+            var size = m_LocaleItems.arraySize;
+            for (int i = 0; i < size; i++)
+            {
+                var enumValueIndex = Array.IndexOf(enumNames, languages[i].ToString());
+                var element = m_LocaleItems.GetArrayElementAtIndex(i);
+                var languageProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleLanguageSerializedProperty);
+                languageProperty.enumValueIndex = enumValueIndex;
             }
         }
     }
