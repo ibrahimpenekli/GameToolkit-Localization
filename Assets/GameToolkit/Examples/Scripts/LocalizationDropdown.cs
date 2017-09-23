3e5ba73a177a9078acc9fba76dbdb9970e9e0fe1
Halil Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2017-09-23 08:47:10 +0300

    Minor improvements.

--- Assets/GameToolkit/Examples/Scripts/LocalizationDropdown.cs
+++ Assets/GameToolkit/Examples/Scripts/LocalizationDropdown.cs
@@ -8,9 +8,17 @@ using GameToolkit.Localization;
 [RequireComponent(typeof(Dropdown))]
 public class LocalizationDropdown : MonoBehaviour
 {
+    [SerializeField]
+    private LocalizedText m_Languages;
+
     private void Start()
     {
-        var options = LocalizationSettings.Instance.AvailableLanguages.Select(x => x.ToString()).ToList();
+        // Write language names as native.
+        var options = LocalizationSettings.Instance.AvailableLanguages.Select(language =>
+        {
+            return m_Languages.GetLocaleValue(language);
+        }).ToList();
+
         var dropdown = GetComponent<Dropdown>();
         dropdown.AddOptions(options);
         dropdown.onValueChanged.AddListener(delegate
