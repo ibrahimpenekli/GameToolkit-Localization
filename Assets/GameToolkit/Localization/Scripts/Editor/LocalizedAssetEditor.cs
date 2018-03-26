18a3313aa92594500d04f83d1ddad148c72f5d9d
H. Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2018-03-27 01:25:09 +0300

    Project updated to 2017.3.1f1.  Localized string editor made expandable.

--- Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
+++ Assets/GameToolkit/Localization/Scripts/Editor/LocalizedAssetEditor.cs
@@ -16,9 +16,18 @@ namespace GameToolkit.Localization.Editor
 
         private ReorderableList m_LocaleItemList;
         private SerializedProperty m_LocaleItems;
+        private Rect m_CurrentLayoutRect;
+        private GUIStyle m_TextAreaStyle;
 
         private void OnEnable()
         {
+            if (m_TextAreaStyle == null)
+            {
+                m_TextAreaStyle = new GUIStyle(EditorStyles.textArea);
+                m_TextAreaStyle.wordWrap = true;
+            }
+
+            var assetValueType = ((LocalizedAssetBase)target).ValueType;
             m_LocaleItems = serializedObject.FindProperty(LocalizationEditorHelper.LocalizedElementsSerializedProperty);
             if (m_LocaleItems != null)
             {
@@ -32,26 +41,47 @@ namespace GameToolkit.Localization.Editor
                     displayRemoveButton: true
                 );
 
-                m_LocaleItemList.drawHeaderCallback = (Rect rect) =>
+                m_LocaleItemList.drawHeaderCallback = (rect) =>
                 {
                     EditorGUI.LabelField(rect, ObjectNames.NicifyVariableName(target.GetType().Name) + "s");
                 };
-
-                m_LocaleItemList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+                m_LocaleItemList.drawElementCallback = (rect, index, isActive, isFocused) =>
                 {
                     var element = m_LocaleItemList.serializedProperty.GetArrayElementAtIndex(index);
 
                     // Language field.
-                    var r1 = new Rect(rect.x, rect.y + 2, LanguageFieldWidth, EditorGUIUtility.singleLineHeight);
-                    EditorGUI.PropertyField(r1, element.FindPropertyRelative(LocalizationEditorHelper.LocaleLanguageSerializedProperty), GUIContent.none);
+                    var languageRect = new Rect(rect.x, rect.y + 2, LanguageFieldWidth, rect.height - 4);
+                    var languageProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleLanguageSerializedProperty);
+                    EditorGUI.PropertyField(languageRect, languageProperty, GUIContent.none);
 
                     // Value field.
-                    var r2 = new Rect(r1.x + r1.width + 4, r1.y, rect.width - r1.width - 4, r1.height);
-                    EditorGUI.PropertyField(r2, element.FindPropertyRelative(LocalizationEditorHelper.LocaleValueSerializedProperty), GUIContent.none);
+                    var valueRect = new Rect(languageRect.x + languageRect.width + 4, languageRect.y, rect.width - languageRect.width - 4, rect.height - 4);
+                    var valueProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleValueSerializedProperty);
+                    if (assetValueType == typeof(string))
+                    {
+                        valueProperty.stringValue = EditorGUI.TextArea(valueRect, valueProperty.stringValue, m_TextAreaStyle);
+                    }
+                    else
+                    {
+                        EditorGUI.PropertyField(valueRect, valueProperty, GUIContent.none);
+                    }
                 };
-                m_LocaleItemList.onCanRemoveCallback = (ReorderableList list) =>
+                m_LocaleItemList.onCanRemoveCallback = (list) =>
                 {
                     return list.count > 1;
+                };
+                m_LocaleItemList.elementHeightCallback = (index) =>
+                {
+                    var element = m_LocaleItemList.serializedProperty.GetArrayElementAtIndex(index);
+                    var valueProperty = element.FindPropertyRelative(LocalizationEditorHelper.LocaleValueSerializedProperty);
+                    var elementHeight = EditorGUIUtility.singleLineHeight;
+
+                    if (assetValueType == typeof(string))
+                    {
+                        var valueWidth = m_CurrentLayoutRect.width - LanguageFieldWidth - 30;
+                        elementHeight = m_TextAreaStyle.CalcHeight(new GUIContent(valueProperty.stringValue), valueWidth);
+                    }
+                    return Mathf.Max(EditorGUIUtility.singleLineHeight, elementHeight) + 4;
                 };
                 m_LocaleItemList.onAddDropdownCallback = (Rect buttonRect, ReorderableList list) =>
                 {
@@ -80,11 +110,15 @@ namespace GameToolkit.Localization.Editor
         {
             if (m_LocaleItemList != null)
             {
+                m_CurrentLayoutRect = GUILayoutUtility.GetRect(0, m_LocaleItemList.GetHeight(), GUILayout.ExpandWidth(true));
                 serializedObject.Update();
-                m_LocaleItemList.DoLayoutList();
+                m_LocaleItemList.DoList(m_CurrentLayoutRect);
                 serializedObject.ApplyModifiedProperties();
 
-                EditorGUILayout.HelpBox("First locale item is used as fallback if needed.", MessageType.Info);
+                var helpRect = m_CurrentLayoutRect;
+                helpRect.y += m_LocaleItemList.GetHeight() + 4;
+                helpRect.height = EditorGUIUtility.singleLineHeight * 1.5f;
+                EditorGUI.HelpBox(helpRect, "First locale item is used as fallback if needed.", MessageType.Info);
             }
             else
             {
