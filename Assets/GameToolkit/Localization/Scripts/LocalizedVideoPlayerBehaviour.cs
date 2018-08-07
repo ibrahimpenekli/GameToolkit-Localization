30d2dcfdd8c8c8106b4643512baccb3819a06f36
Mysterious-Devs <40738104+Mysterious-Devs@users.noreply.github.com>
2018-08-07 13:57:42 +0200

    Create LocalizedVideoPlayerBehaviour.cs

---
+++ Assets/GameToolkit/Localization/Scripts/LocalizedVideoPlayerBehaviour.cs
@@ -0,0 +1,31 @@
+// Copyright (c) H. Ibrahim Penekli. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace GameToolkit.Localization
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [RequireComponent(typeof(VideoPlayer))]
+    public class LocalizedVideoPlayerBehaviour : LocalizedAssetBehaviour
+    {
+        
+        public LocalizedVideoPlayer LocalizedTexture;
+
+        protected override void UpdateComponentValue()
+        {
+            if (LocalizedTexture)
+            {
+                GetComponent<VideoPlayer>().clip = LocalizedTexture.Value;
+            }
+        }
+
+       /* private void OnValidate()
+        {
+            MaterialIndex = Mathf.Max(0, MaterialIndex);
+        }*/
+    }
+}
